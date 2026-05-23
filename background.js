505844348485f22e3367244b50907e62fe6dcfcd
John <roblox888i12345@gmail.com>
2026-05-23 14:05:14 -0400

    Add listener to inject CSS on specific tab updates

--- background.js
+++ background.js
@@ -1,1 +1,17 @@
-console.log("done");
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && tab.url) {
+        if (tab.url.includes("scratch.mit.edu") || tab.url.includes("scratch.org")) {
+      
+      chrome.scripting.insertCSS({
+        target: { tabId: tabId },
+        files: ["site-css/components.css"]
+      })
+      .then(() => {
+        console.log(`done`);
+      })
+      .catch((err) => {
+        console.error("error:", err);
+      });
+    }
+  }
+});
