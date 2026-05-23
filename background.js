f47638f36cdd7223db5783493e28630b5a09a41a
John <roblox888i12345@gmail.com>
2026-05-23 13:47:09 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,1 @@
-chrome.action.onClicked.addListener((tab) => {
-  if (tab.url?.startsWith('http')) {
-    
-    chrome.scripting.insertCSS({
-      target: { tabId: tab.id },
-      files: ["imports.css"]
-    })
-    .then(() => console.log("done"))
-    .catch((err) => console.error("failed:", err));
-
-  }
-});
+console.log("done");
