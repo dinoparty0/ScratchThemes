9b0d3a9d960cf9c6845d318c81cf448ff369ae65
John <roblox888i12345@gmail.com>
2026-05-24 11:45:54 -0400

    Update components.js

--- site-js/components.js
+++ site-js/components.js
@@ -1,21 +1,22 @@
 /* JS for components across the Scratch site (e.g. banners, world map) */
 
-function removeComponents() {
-const studentBanner = document.querySelectorAll('.student-deactivation-banner');
+(function() {
+  function removeComponents() {
+    let studentBanners = document.querySelectorAll('.student-deactivation-banner');
+    
+    for (var i = 0; i < studentBanners.length; i++) {
+      studentBanners[i].remove();
+    }
+  }
 
-studentBanner.forEach(function(el) {
-  el.remove('.student-deactivation-banner');
-  });
-  
-}
-
-removeComponents();
-
-const observer = new MutationObserver(function(mutations) {
   removeComponents();
-});
 
-observer.observe(document.body, {
- childList: true,
- subtree: true
-});
+  var observer = new MutationObserver(function(mutations) {
+    removeComponents();
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  });
+})();
