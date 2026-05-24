1b3abd71364fa07a277933b5e5e1ba3743ac5c2e
John <roblox888i12345@gmail.com>
2026-05-24 10:59:43 -0400

    chore: add JS injection

--- background.js
+++ background.js
@@ -1,17 +1,21 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete' && tab.url) {
-        if (tab.url.includes("scratch.mit.edu") || tab.url.includes("scratch.org")) {
+    if (tab.url.includes("scratch.mit.edu") || tab.url.includes("scratch.org")) {
       
       chrome.scripting.insertCSS({
         target: { tabId: tabId },
         files: ["site-css/components.css"]
       })
-      .then(() => {
-        console.log(`done`);
+      .then(() => console.log("done css"))
+      .catch((err) => console.error("error with css:", err));
+
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        files: ["site-js/components.js"]
       })
-      .catch((err) => {
-        console.error("error:", err);
-      });
+      .then(() => console.log("done js"))
+      .catch((err) => console.error("error with js:", err));
+      
     }
   }
 });
